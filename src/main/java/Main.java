9b5bc3694ed64ff3a309ba00c50bc2e042c1d83f
Vinicius Pacheco <viniciusmpacheco22@gmail.com>
2024-11-29 14:47:07 -0300

    add compress content

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,13 @@
 import org.apache.commons.codec.digest.DigestUtils;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 public class Main {
 
@@ -23,14 +28,36 @@ public class Main {
 
     public static String getFileHash(String fileContent){
         int fileContentByteSize = fileContent.getBytes().length;
-        String headerAndContent = "commit " + fileContentByteSize + "\u0000" + fileContent;
+        String header = "object " + fileContentByteSize + "\u0000";
+        String headerAndContent =  header + fileContent;
         return DigestUtils.sha1Hex(headerAndContent);
+    }
+
+    public static byte[] compress(byte[] input) {
+        Deflater deflater = new Deflater();
+        deflater.setInput(input);
+        deflater.finish();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+
+        while (!deflater.finished()) {
+            int compressedSize = deflater.deflate(buffer);
+            outputStream.write(buffer, 0, compressedSize);
+        }
+
+        return outputStream.toByteArray();
     }
 
     public static void main(String[] args) {
         try {
             String fileContent = readFile();
             String fileHash = getFileHash(fileContent);
+            byte[] compressedContent = compress(fileContent.getBytes(StandardCharsets.UTF_8));
+
+            String str = new String(compressedContent, StandardCharsets.UTF_8);
+
+            System.out.println(str);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
