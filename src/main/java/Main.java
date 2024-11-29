d988c004003b8eea6563b7f094b8923e4fb68dec
Vinicius Pacheco <viniciusmpacheco22@gmail.com>
2024-11-29 12:19:01 -0300

    add getFileHash

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import org.apache.commons.codec.digest.DigestUtils;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -19,10 +21,16 @@ public class Main {
         return fullContent.toString();
     }
 
+    public static String getFileHash(String fileContent){
+        int fileContentByteSize = fileContent.getBytes().length;
+        String headerAndContent = "commit " + fileContentByteSize + "\u0000" + fileContent;
+        return DigestUtils.sha1Hex(headerAndContent);
+    }
+
     public static void main(String[] args) {
         try {
             String fileContent = readFile();
-            System.out.println(fileContent);
+            String fileHash = getFileHash(fileContent);
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
